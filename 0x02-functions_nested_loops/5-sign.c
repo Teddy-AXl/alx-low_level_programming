943c941001e17045644a3853d59ad01e5ee551c7
Teddy_AXl <teddy.ntawe@gmail.com>
2023-07-18 17:00:14 -0700

    5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,24 +2,27 @@
 
 /**
  * print_sign - prints the sign of a number
- * @n: The number to be  checked
- * Return: 1 for positive num,  -1 for negative num or zero for anything else 
+ * @n: The number to be checked
+ *
+ * Return: 1 for positive num, -1 for negative num, or zero for anything else
  */
 int print_sign(int n)
 {
-    if (n > 0)
-    {
-    _putchar(43);
-    return (1);
-    }
-    else if (n <  0)
-    { 
-    _putchar(45);
-    return (-1);
-    }
-    else
-    {   
-    _putchar(48);
-    return (0);
-    }
+	/* Check if the number is greater than zero */
+	if (n > 0)
+	{
+		_putchar(43); /* Print positive sign */
+		return (1);
+	}
+	/* Check if the number is less than zero */
+	else if (n < 0)
+	{
+		_putchar(45); /* Print negative sign */
+		return (-1);
+	}
+	else
+	{
+		_putchar(48); /* Print zero */
+		return (0);
+	}
 }
