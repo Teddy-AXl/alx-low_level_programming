8992a6d99f3c78f720373c539cbe3d22d4988bc9
Teddy_AXl <teddy.ntawe@gmail.com>
2023-08-08 10:18:55 -0700

    6

---
+++ 0x0B-malloc_free/101-main.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_tab - Prints an array of strings.
+ * @tab: The array to print.
+ *
+ * Return: Nothing.
+ */
+void print_tab(char **tab)
+{
+	int i;
+
+	for (i = 0; tab[i] != NULL; ++i)
+	{
+		printf("%s\n", tab[i]);
+	}
+}
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	char **tab;
+
+	tab = strtow("      ALX School         #cisfun      ");
+	if (tab == NULL)
+	{
+		printf("Failed\n");
+		return (1);
+	}
+	print_tab(tab);
+	return (0);
+}
