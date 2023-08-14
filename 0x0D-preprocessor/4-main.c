aca8ddae02382a4e0f0e31d20b4ac16d76f85d4b
Teddy_AXl <teddy.ntawe@gmail.com>
2023-08-14 02:56:28 -0700

    4

---
+++ 0x0D-preprocessor/4-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "4-sum.h"
+#include "4-sum.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Checks the SUM macro by calculating the sum of two numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int s;
+
+	s = SUM(98, 1024);
+	printf("%d\n", s);
+
+	return (0);
+}
