2f0915d6832dae705ef4eb7d46d3da9fc1a8ee53
Teddy_AXl <teddy.ntawe@gmail.com>
2023-08-14 02:41:08 -0700

    0

---
+++ 0x0D-preprocessor/0-main.c
@@ -0,0 +1,20 @@
+#include "0-object_like_macro.h"
+#include "0-object_like_macro.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the result of a calculation involving SIZE
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int s;
+
+	s = 98 + SIZE;
+	printf("%d\n", s);
+
+	return (0);
+}
