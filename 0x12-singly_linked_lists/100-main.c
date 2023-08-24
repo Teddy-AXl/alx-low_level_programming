efc6eb0b81b77df66707c674588d6a40c4d37059
Teddy_AXl <teddy.ntawe@gmail.com>
2023-08-24 14:46:09 -0700

    single linked task files

---
+++ 0x12-singly_linked_lists/100-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("(A tortoise, having a pretty good sense of a hare's nature,");
+	printf(" challenges one to a race.)\n");
+	return (0);
+}
